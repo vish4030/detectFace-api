b7570a1dfb34f3b4924588e16f07d6e28490ae19
amit kumar <amitkumar@amits-MacBook-Air.local>
2021-06-08 23:52:30 +0530

    adding database

--- server.js
+++ server.js
@@ -12,10 +12,8 @@ const express = require('express');
 const db = knex({
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
-    user : '',
-    password : '',
-    database : 'smart-brain'
+    host : process.env.DATABASE_URL,
+    ssl: true,
   }
 })
 
